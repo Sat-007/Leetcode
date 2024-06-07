95ca283061800e216ee49ab7c8a95117b98e7d26
Sathvik Kumar <49225817+Sat-007@users.noreply.github.com>
2024-06-07 15:39:37 -0500

    Time: 45 ms (5.28%), Space: 16.4 MB (94.36%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,8 +1,8 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = []
+        stack = [] 
         paren = {")":"(","}":"{","]":"["}
-
+        
         for p in s:
             if p in paren.values():
                 stack.append(p)
