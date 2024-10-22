dfc83f73f4373e83a8420cffc1b7d6ff695b791e
Sathvik Kumar <49225817+Sat-007@users.noreply.github.com>
2024-10-22 11:57:47 -0500

    Time: 9 ms (97.25%), Space: 16.8 MB (6.58%) - LeetHub

--- 0811-subdomain-visit-count/0811-subdomain-visit-count.py
+++ 0811-subdomain-visit-count/0811-subdomain-visit-count.py
@@ -1,18 +1,19 @@
 class Solution:
     def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
-        visit_counts = {}
-        
+        visit = {}
         for cpdomain in cpdomains:
-            count, domain = cpdomain.split()
-            count = int(count)
-            subdomains = domain.split('.')
-            #print(subdomains)
+            cpdomain = cpdomain.split()
+            count = int(cpdomain[0])
+            subdomains = cpdomain[1].split('.')
+            #print(count)
+            #print(subdomain)
             for i in range(len(subdomains)):
-                subdomain = '.'.join(subdomains[i:])
-                if subdomain not in visit_counts:
-                    visit_counts[subdomain] = 0
-                visit_counts[subdomain] += count
-                
-        res = [f"{count} {subdomain}" for subdomain, count in visit_counts.items()]
+                subdomain = ".".join(subdomains[i:])
+                if subdomain not in visit:
+                    visit[subdomain] = 0 
+                visit[subdomain] += count
+        res = [f"{count} {subdomain}" for subdomain, count in visit.items()]
         return res
-                    
+                
+                
+        
