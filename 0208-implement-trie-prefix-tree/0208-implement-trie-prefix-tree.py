d68a5a9d14ac4badead3d1c5a9e816550e6b6537
Sathvik Kumar <49225817+Sat-007@users.noreply.github.com>
2024-11-14 15:06:12 -0600

    Time: 46 ms (61.42%), Space: 32 MB (27.63%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -0,0 +1,32 @@
+from typing import Dict, Optional
+
+class TrieNode:
+    def __init__(self):
+        self.children: Dict[str, 'TrieNode'] = {}
+        self.isWord = False
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for c in word:
+            node = node.children.setdefault(c, TrieNode())
+        node.isWord = True
+
+    def search(self, word: str) -> bool:
+        node = self._find(word)
+        return node is not None and node.isWord
+
+    def startsWith(self, prefix: str) -> bool:
+        return self._find(prefix) is not None
+
+    def _find(self, prefix: str) -> Optional[TrieNode]:
+        node = self.root
+        for c in prefix:
+            if c not in node.children:
+                return None
+            node = node.children[c]
+        return node
