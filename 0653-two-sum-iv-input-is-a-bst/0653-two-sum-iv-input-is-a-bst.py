298f3496f18bfdc1a69214a2aeb507d6283a8d87
Sathvik Kumar <49225817+Sat-007@users.noreply.github.com>
2024-11-14 18:26:03 -0600

    Time: 0 ms (100.00%), Space: 18.8 MB (37.92%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
@@ -1,33 +1,11 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
-        
-        
-        def in_order(root, values):
-            if root:
-                in_order(root.left, values)
-                values.append(root.val)
-                in_order(root.right, values)
-                
-        values = []
-        in_order(root, values)        
-            
-        l = 0 
-        r = len(values) - 1
-
-        while l < r:
-            summ = values[l] + values[r]
-            if summ == k:
+        def helper(root):
+            if not root:
+                return False
+            if root.val in seen:
                 return True
-            elif summ < k:
-                l += 1
-            else:
-                r -= 1
-        return False
-
-       
+            seen[k-root.val] = 1
+            return helper(root.left) or helper(root.right)
+        seen = {}
+        return helper(root)
